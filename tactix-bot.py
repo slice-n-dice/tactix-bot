12c5699617484dc0719ccc037787ca642ecbaa61
slice-n-dice <51350635+slice-n-dice@users.noreply.github.com>
2021-10-19 21:01:33 -0700

    Add runeword commands
    Added functionality to look up runeword information, and search for which runewords use a given rune.

--- tactix-bot.py
+++ tactix-bot.py
@@ -1,30 +1,44 @@
 # tactix-bot.py
 
 import os
+import random
 import json
 import discord
 from discord.ext import commands
 intents = discord.Intents.default()
 intents.members = True # This allows us to see a list of members in guild.
 
+EMBED_COLOR = 0x1c3818
+
 from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 # Save all rune data in an object
-f = open('runes.json')
+f = open("runes.json")
 rune_data_raw = json.load(f) # this is a dict with 1 key-value pair
 # key "runelist"; value is ALL rune info in a list of dicts
 f.close()
 
+# Save all runeword data in an object
+f = open("runewords.json")
+runeword_data_raw = json.load(f) # this is a dict with 1 key-value pair
+# key "runewords"; value is ALL runeword info in a list of dicts
+f.close()
+
 rune_data = rune_data_raw["runelist"] # this is a list of dicts; 1 dict per rune
+runeword_data = runeword_data_raw["runewords"]
 
 # Generate rune list
 rune_list = []
 for i in rune_data:
     rune_list.append(i["rune_name"])
 
-#client = discord.Client()
+# Generate runeword list
+runeword_list = []
+for i in runeword_data:
+    runeword_list.append(i["name"].lower())
+
 bot = commands.Bot(command_prefix='!')
 
 @bot.command()
@@ -35,7 +49,7 @@ async def about(ctx):
     description="Created by tentacles for TactiX's War Room.",
     color=0x1c3818)
     url = "http://jameskennethnelson.com/discord_bot/tactix-bot/images/bot-avatar.png"
-    embed.set_thumbnail(url=url)
+    embed.set_image(url=url)
     await ctx.send(embed=embed)
 
 @bot.command()
@@ -53,11 +67,13 @@ async def runelist(ctx):
     s_low = t.join(low_runes)
     s_mid = t.join(mid_runes)
     s_high = t.join(high_runes)
-    embed = discord.Embed(title="Rune List", color=0x1c3818)
+    embed = discord.Embed(title="Rune List", color=EMBED_COLOR)
     embed.add_field(name="Low Runes", value=s_low)
     embed.add_field(name="Mid Runes", value=s_mid)
     embed.add_field(name="High Runes", value=s_high)
-    embed.set_footer(text="Use !runeinfo for more detail on any rune.\nExample: '!runeinfo lem'")
+    random_rune = random.choice(rune_list)
+    embed.set_footer(text="Use !runeinfo for more detail on any rune.\n" + \
+        "Example: !runeinfo " + random_rune)
     await ctx.send(embed=embed)
 
 @bot.command()
@@ -72,11 +88,48 @@ async def runeinfo(ctx, r):
         embed = build_rune_embed(rune, d) # construct embed
         await ctx.send(embed=embed)
 
+@bot.command()
+async def runeword(ctx, *runeword):
+    '''Prints information on the given Diablo 2 runeword.'''
+    # Glue together the potentially multi-word argument
+    r = " ".join(runeword)
+    r = r.lower() # Match capitalization of runeword list object
+    if r not in runeword_list:
+        await ctx.send("That runeword does not exist.")
+    else:
+        i = runeword_list.index(r)
+        d = runeword_data[i]
+        embed = build_runeword_embed(r, d)
+        await ctx.send(embed=embed)
+
+@bot.command()
+async def runesearch(ctx, rune):
+    '''Returns a list of runewords that use the given rune.'''
+    c_rune = rune.lower().capitalize()
+    runeword_list = []
+    for r in runeword_data:
+        if c_rune in r["runes"]:
+            t = clean_capitalize(r["name"])
+            runeword_list.append(t)
+
+    if not runeword_list:
+        await ctx.send("Did not find any runewords. Are you sure you're searching " + \
+            "for a valid rune? Use !runelist for a full list.")
+        return
+
+    embed = discord.Embed(title="Runewords With "+c_rune, color=EMBED_COLOR,
+        description=", ".join(runeword_list))
+    random_runeword = random.choice(runeword_list)
+    embed.set_footer(text="Use !runeword for more info on any runeword.\n" + \
+        "Example: !runeword " + random_runeword)
+    await ctx.send(embed=embed)
+
+
 def build_rune_embed(rune, data):
     '''Creates an embed object based on the given rune and its data.'''
     # rune - string, capitalized
     # data - list containing the rune information
-    embed = discord.Embed(title=rune+" Rune", color=0x1c3818)
+    embed = discord.Embed(title=rune+" Rune", color=EMBED_COLOR)
     url = "http://jameskennethnelson.com/discord_bot/tactix-bot/images/runes/"+rune+".PNG"
     embed.set_thumbnail(url=url)
     embed.add_field(name="Weapon Effect", value=data["weapon_effect"])
@@ -87,5 +140,28 @@ def build_rune_embed(rune, data):
     embed.add_field(name="Previous Rune", value=data["upgrade_from"])
     embed.add_field(name="Next Rune", value=data["upgrade_to"])
     return embed
+
+def build_runeword_embed(runeword, data):
+    '''Creates an embed object based on the given runeword and its data.'''
+    # runeword - string, all words capitalized
+    # data = list containing the runeword information
+   
+    title = clean_capitalize(runeword)
+    embed = discord.Embed(title=title, color=EMBED_COLOR)
+    runes = "".join(data["runes"])
+    embed.add_field(name="Runeword", value=runes)
+    gear = ", ".join(data["gear_types"])
+    embed.add_field(name="Gear Types", value=gear)
+    embed.add_field(name="Character Level Required", value=data["clvl"])
+    attributes = "\n".join(data["attributes"])
+    embed.add_field(name="Attributes", value=attributes, inline=False)
+    return embed
+
+def clean_capitalize(s):
+    '''Returns a string where all words are capitalized.
+    Letters immediately following symbols (like apostrophe) are NOT treated
+    as new words, unlike the default python string.capitalize() function.'''
+    t = [word.capitalize() for word in s.split()]
+    return " ".join(t)
 
 bot.run(TOKEN)
